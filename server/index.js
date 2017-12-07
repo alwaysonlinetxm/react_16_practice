ab4f78ad1255e5f66eaa64e90d38a1bec2603cd3
alwaysonlinetxm <alwaysonlinetxm@163.com>
2017-12-07 20:05:29 +0800

    update server

--- server/index.js
+++ server/index.js
@@ -1,45 +1,20 @@
-const Koa = require("koa");
-const app = new Koa();
-const router = require('koa-better-router')().loadMethods();
-const cheerio = require("cheerio");
-const fs = require("fs");
-const path = require("path");
-// const Promise = require("bluebird");
-const serve = require('koa-static-server');
-// const readFileAsync = Promise.promisify(fs.readFile);
-const RES_PATH = path.resolve(__dirname, '../dist/');
-// const axios = require("axios");
-
-//////////////////////////////////////////////////////
-
-// const React = require("react");
+import Koa from 'koa';
+import Router from 'koa-better-router';
+import staticServer from 'koa-static-server';
+import cheerio from 'cheerio';
+import fs from 'fs';
+import path from 'path';
 import React from 'react';
-const ReactDOMServer = require("react-dom/server");
-const { renderToString } = ReactDOMServer;
-
-
-const reactRouter = require('react-router');
-import { createAppRoutes, createAppStore } from '../dist_server/index.ssr';
+import ReactDOMServer from 'react-dom/server';
+import { match, RouterContext } from 'react-router'
 import { Provider } from 'react-redux';
 
-const { match, RouterContext } = reactRouter;
+import { createAppRoutes, createAppStore } from '../dist_server/index.ssr';
 
-
-/**
- * 读取HTML模版，返回cheerio实例
- * @param path
- * @return {Promise.<*>}
- */
- function loadHTMLTemplate(path) {
-    try {
-        let content = fs.readFileSync(path);
-        return cheerio.load(content);
-
-    } catch (e) {
-        console.error(e);
-        return false;
-    }
-}
+const app = new Koa();
+const router = Router().loadMethods();
+const { renderToString } = ReactDOMServer;
+const ROOT_PATH = path.resolve(__dirname, '../dist/');
 
 router.get('*', async (ctx, next) => {
   // create a new store everytime for not sharing the same store among different users
@@ -50,49 +25,52 @@ router.get('*', async (ctx, next) => {
     })
   });
 
-  // console.log(props);
-
   if (props) {
-    const { getData } = props.routes[1];
-    const $ = loadHTMLTemplate(path.resolve(RES_PATH, 'index.html'));
-    let initState = {}
+    try {
+      const { getData } = props.routes[1];
+      const $ = cheerio.load(fs.readFileSync(path.resolve(ROOT_PATH, 'index.html')));
+      let initState = {}
 
-    // get initState
-    if (typeof getData === 'function') {
-      await Promise.all(getData());
-      initState = store.getState();
+      // get initState
+      if (typeof getData === 'function') {
+        await Promise.all(getData());
+        initState = store.getState();
+      }
+
+      const appHtml = renderToString(
+        <Provider store={ store }>
+          <RouterContext {...props}/>
+        </Provider>
+      );
+      $('#app').html(appHtml);
+      $('head').append(`<script>window.SERVER_INIT_STATE=${JSON.stringify(initState)}</script>`);
+
+      return ctx.body = $.html();
+    } catch(e) {
+      console.error(' ---- server render error !! ----');
+      console.error(e);
     }
-
-    console.log('-----', initState)
-
-    const appHtml = renderToString(
-      <Provider store={ store }>
-        <RouterContext {...props}/>
-      </Provider>
-    );
-    $('#app').html(appHtml);
-    $('head').append(`<script>window.SERVER_INIT_STATE=${JSON.stringify(initState)}</script>`);
-
-    return ctx.body = $.html();
   }
+  // other middles will still run without 'next()'
+  // return next();
 });
 
 //router
 app.use(router.middleware());
 
-//router
+// determine to run staticServer or not
 app.use(function (ctx, next) {
-
-    //如果路由中间件已经有数据了，无需再走静态文件中间件了
-    if (ctx.body) {
-        return true;
-    }
-
-    return next();
+  if (ctx.body) {
+    return true;
+  }
+  return next();
 });
 
-app.use(serve({rootDir: RES_PATH}));
+// static server
+app.use(staticServer({
+  rootDir: ROOT_PATH
+}));
 
-app.listen(8088, _ => {
-    console.log('server started... http://localhost:8088/')
+app.listen(1207, _ => {
+  console.log('---- start server at: http://localhost:1207/ ----');
 });
